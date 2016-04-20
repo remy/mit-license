beb8ec144c85bea20d5fe2a4b61e9e295321d9e1
Remy Sharp <remy@remysharp.com>
2016-04-20 11:00:37 +0100

    fix: move to https

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 #!/bin/env node
+
 var fs = require('fs');
-var users = fs.readdirSync("users");
-for(i in users){
-	try{
-		var content = fs.readFileSync("users/"+users[i]).toString();
-		JSON.parse(content);
-	}
-	catch(e){
-		console.error("Invalid JSON in file: " + users[i]);
-		process.exit(1);
-	}
-}
+var users = fs.readdirSync('users');
+users.forEach(function (user) {
+  try {
+    var content = fs.readFileSync('users/' + user).toString();
+    JSON.parse(content);
+  } catch (e) {
+    console.error('Invalid JSON in file: ' + user);
+    process.exit(1);
+  }
+});
