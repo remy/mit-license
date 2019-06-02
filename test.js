1d65967dfccd024c39ccf1eb88b351e1655009dc
Richie Bendall <richiebendall@gmail.com>
2019-06-02 18:15:57 +1200

    Update tests and remove offending file

--- test.js
+++ test.js
@@ -8,23 +8,28 @@ let errored = false
 const users = fs.readdirSync('users')
 users.forEach(async (user) => {
     if (user.endsWith('json')) {
-        fs.readFile(path.join('users', user), 'utf8', async (err, content) => {
-            if (err) {
-                errored = true
-                console.error(`Unable to read ${user}`)
-            } else {
-                try {
-                    const u = JSON.parse(content)
-                    if (!u.locked && !u.copyright) {
-                        errored = true
-                        console.error(`Copyright not specified in ${user} (${e})`)
-                    }
-                } catch (e) {
+        if (encodeURIComponent(user) === user) {
+            fs.readFile(path.join('users', user), 'utf8', async (err, content) => {
+                if (err) {
                     errored = true
-                    console.error(`Invalid JSON in ${user} (${e})`)
+                    console.error(`Unable to read ${user}`)
+                } else {
+                    try {
+                        const u = JSON.parse(content)
+                        if (!u.locked && !u.copyright) {
+                            errored = true
+                            console.error(`Copyright not specified in ${user} (${e})`)
+                        }
+                    } catch (e) {
+                        errored = true
+                        console.error(`Invalid JSON in ${user} (${e})`)
+                    }
                 }
-            }
-        })
+            })
+        } else {
+            errored = true
+            console.error(`${user} is not URL safe`)
+        }
     } else {
         errored = true
         console.error(`${user} is not a json file`)
